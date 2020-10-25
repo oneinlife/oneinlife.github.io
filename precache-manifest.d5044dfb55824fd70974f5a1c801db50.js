cd4f6e56161011789ecb3ac069bfdb1d5b58d1ca
Pustovit <d.s.pustovit@m9.etagi.com>
2020-10-25 17:42:24 +0500

    initial

---
+++ precache-manifest.d5044dfb55824fd70974f5a1c801db50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d9311e49c87e70d9f69fe3534e95287",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7cd252524db188a0b7f",
+    "url": "/static/css/main.dc685032.chunk.css"
+  },
+  {
+    "revision": "b143d55166edaf8267a1",
+    "url": "/static/js/2.d2b32858.chunk.js"
+  },
+  {
+    "revision": "6bbb3a8e7360a6a98672414629b231d4",
+    "url": "/static/js/2.d2b32858.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7cd252524db188a0b7f",
+    "url": "/static/js/main.adbefb33.chunk.js"
+  },
+  {
+    "revision": "c286cb8f254b8e20e78e",
+    "url": "/static/js/runtime-main.c3ae6158.js"
+  }
+]);
